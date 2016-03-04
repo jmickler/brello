04522bbeedaa9ba8f93c2a0eb9d19e0aefdfdbce
jmickler <jhmickler@gmail.com>
2016-03-03 18:48:28 -0600

    add models and migrations

---
+++ database/migrations/2016_03_04_001053_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('lane_id')->unsigned()->index();
+            $table->string('title');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
